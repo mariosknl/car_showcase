6ee89cf1ca65332d0ee99da42268013e9b6cc8f0
Marios Kanellopoulos <mariosknl@outlook.com>
2023-06-20 17:02:16 +0300

    card details modal created

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
 	car: CarProps;
@@ -73,6 +74,12 @@ const CarCard = ({ car }: CarCardProps) => {
 					/>
 				</div>
 			</div>
+
+			<CarDetails
+				isOpen={isOpen}
+				closeModal={() => setIsOpen(false)}
+				car={car}
+			/>
 		</div>
 	);
 };
